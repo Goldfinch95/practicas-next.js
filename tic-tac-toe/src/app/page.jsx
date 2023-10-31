b5745d7c246c3e0bfba79be22a9db3fc099f1669
Goldfinch <facu_vila@hotmail.com>
2023-10-31 16:13:42 -0300

    se agrego una ventana de victoria o empate y un boton de reseteo del juego

--- tic-tac-toe/src/app/page.jsx
+++ tic-tac-toe/src/app/page.jsx
@@ -9,7 +9,7 @@ const PLAYERS = {
   X: "X",
   O: "O"
 }
-
+//combinaciones ganadoras
 const WINNER_COMBOS = [
   [0,1,2],
   [3,4,5],
@@ -29,13 +29,13 @@ export default function Home() {
   const [turn, setTurn] = useState(PLAYERS.X)
   //declarar ganador, null no hay ganador, false hay empate.
   const [winner, setWinner] = useState(null)
-
+  //comprabar ganador
   const checkWinner = (boardToCheck)=>{
     for(const combo of WINNER_COMBOS){
       const [a,b,c] = combo
       if(
         boardToCheck[a] && // 0 -> X u O
-        boardToCheck[a] == boardToCheck[b] &&  // 0 Y 3 -> X -> X u O -> O
+        boardToCheck[a] == boardToCheck[b] &&  // 0 Y 1 -> X -> X u O -> O
         boardToCheck[a] == boardToCheck[c]
       ){
         return boardToCheck[a] // ganador X u O
@@ -44,6 +44,17 @@ export default function Home() {
     //si no hay ganador
     return null
   }
+
+ //reiniciar el juego, reseteando el estado.
+ const resetGame = () => {
+  setBoard(Array(9).fill(null))
+  setTurn(PLAYERS.X)
+  setWinner(null)
+ }
+ const checkEndGame = (newBoard) => {
+  //revisamos si hay empate, si no hay mas espacios vacios en el tablero.
+  return newBoard.every((square) => square !== null)
+ }
   //actualizar el turno
   const updateBoard = (index) =>{
     //no actualizamos esta posicion, si ya tiene algo o ya hay un ganador
@@ -59,12 +70,14 @@ export default function Home() {
     const newWinner = checkWinner(newBoard)
     if(newWinner){
       setWinner(newWinner)
-      alert(`El ganador es ${newWinner}`)
+    } else if (checkEndGame(newBoard)){
+      setWinner(false)
     }
   }
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reiniciar el juego</button>
       <section className='game'>
         {/*se renderiza una lista de elementos*/}
         {board.map((square,index)=>{
@@ -85,6 +98,28 @@ export default function Home() {
         <Square isSelected ={turn == PLAYERS.X}>{PLAYERS.X}</Square>
         <Square isSelected ={turn == PLAYERS.O}>{PLAYERS.O}</Square>
       </section>
+
+      {
+        winner !== null && (
+          <section className='winner'>
+            <div className='text'>
+              <h2>
+                {
+                  winner == false ? 'Empate' : 'Gano:'
+                }
+              </h2>
+              <header className='win'>
+                {winner && <Square>{
+                  winner}
+                  </Square>}
+              </header>
+              <footer>
+              <button onClick={resetGame}>Empezar de nuevo</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
     </main>
   )
 }
